58010fc4f0c166dc13341fb8a8e0e5dc0d18fa7a
Rico474 <brilleeric24@gmail.com>
2025-07-01 19:06:48 +0200

    Ajoute next.config.js pour activer App Router

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // Active App Router
+  },
+}
+
+module.exports = nextConfig;
